7d7bbdfe2008094d55e6988703e4872941957b04
RayFreedom88 <captain.musin@yandex.ru>
2023-09-12 17:13:11 +0200

    fix: убирает лишнее

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,9 +2,6 @@ export const URL_API = 'https://api.openweathermap.org/data/2.5/';
 export const API_KEY = 'd7bf260fb0f276e0731eb04e214d5cda';
 export const DEFAULT_CITY = 'москва';
 
-// запасной API_KEY, если вдруг один из ключей заблокиркуют из-за превышения запросов
-// export const API_KEY = "1eb5d177c062fccb2758fa557651791d";
-
 export const mockDataError = {
   id: 42,
   weather: [{ description: 'пасмурно' }],
